fb430097c08ec321e61fb9011ee81ed88eaad07b
gerry-gqj <gerry_gqj@163.com>
2022-07-24 20:45:57 +0800

    # add spring-cloud bus

---
+++ cloud-config-client-3366/src/main/java/com/qibria/cloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.qibria.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope //用于动态刷新配置文件
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/info")
+    public String configInfo(){
+        return "config info:"+configInfo + "\tport: " + serverPort;
+    }
+
+
+}
